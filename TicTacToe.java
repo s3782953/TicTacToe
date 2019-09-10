63fcc2d04906d5ad96afefd11a2990c32710f84e
s3782953 <s3782953@student.rmit.edu.au>
2019-09-11 02:43:44 +1000

    Finished TicTacToe Full working Computer

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,16 +5,21 @@ public class TicTacToe {
     private static int column = 3; // The Column Height
     private static int[][] plane = new int[row][column]; // The main array
     private static int[][] marker = new int[row][column]; // The copy array
+    private static int[][] filler = new int[row][column];
     private static boolean finish = true; // Checks if all the spaces are filled
     private static boolean flick = false; // Flicks true once the computer made a move
+    private static boolean tick = false;
+    private static boolean another = false;
+    private static boolean win = false;
 
     public static void main(String[] args) { // The main method
         while (finish) { // Runs until the finish variable is flase
             player(); // The players move
             complete(); // Checks if all spaces are filled
-            algorithm(); // The computers move
             Check();
+            algorithm(); // The computers move
             status(); // Prints out the status of the game
+            win();
         }
     }
 
@@ -29,11 +34,11 @@ public class TicTacToe {
         plane[user_row - 1][user_col - 1] = 1;
     }
 
-    private static void algorithm() { //The computers algorithm
-        while (flick == false && finish == true) {
+    private static void algorithm() { // The computers algorithm
+        while (flick == false && finish == true && tick == false) {
             int newrow = 0;
             int newcol = 0;
-            double random = Math.round(Math.random() * 8); //Chooses a number bewtween 0-8 (since arrays starts at 0)
+            double random = Math.round(Math.random() * 8); // Chooses a number bewtween 0-8 (since arrays starts at 0)
             System.out.println(random);
             for (int i = 0; i < random; i++) {
                 newcol++;
@@ -47,11 +52,44 @@ public class TicTacToe {
                 flick = true;
             }
         }
+        clear();
         flick = false;
+        tick = false;
     }
 
-    private static void Check() { //Checks if there are any nearby wins
+    private static void Check() { // Checks if there are any nearby wins
+        int counter = 0;
+        int xcounter = 0;
+        boolean again = true;
+        row();
+        col();
+        dwnDiag();
+        upDiag();
 
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (marker[i][j] == 1) {
+                    counter++;
+                    another = true;
+                }
+            }
+        }
+        while (again == true && another == true) {
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    if (marker[i][j] == 1 && again == true) {
+                        double random = Math.round(Math.random() * (counter - 1) + 1);
+                        xcounter++;
+                        if (random == xcounter) {
+                            plane[i][j] = 2;
+                            tick = true;
+                            again = false;
+                        }
+                    }
+                }
+            }
+            xcounter = 0;
+        }
     }
 
     private static void complete() { // Checks if the game is finish once all spaces are filled out
@@ -66,6 +104,7 @@ public class TicTacToe {
         if (counter == 0) {
             finish = false;
         }
+
     }
 
     private static void status() { // Prints out the status of the game
@@ -76,4 +115,148 @@ public class TicTacToe {
             }
         }
     }
+
+    private static void win() { //Checks if player has won
+        if (win == true) {
+            finish = false;
+            System.out.println("PLAYER WINS");
+        }
+        
+    }
+
+    private static void row() { //Checks all rows for any nearby wins
+        int counter = 0;
+        int nrow = 0;
+        int ncol = 0;
+        int i = 0;
+        int j = 0;
+        boolean done = true;
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < column; j++) {
+                if (plane[i][j] == 1) {
+                    counter++;
+                }
+                if (plane[i][j] == 0) {
+                    nrow = i;
+                    ncol = j;
+                }
+                if (plane[i][j] == 2) {
+                    done = false;
+                }
+            }
+            if (done == true && counter > 1) {
+                marker[nrow][ncol] = 1;
+            }
+            if (counter > 2) {
+                win = true;
+            }
+            counter = 0;
+            done = true;
+        }
+    }
+
+    private static void col() { //Checks all columns for nearby wins
+        int counter = 0;
+        int nrow = 0;
+        int ncol = 0;
+        int i = 0;
+        int j = 0;
+        boolean done = true;
+        for (j = 0; j < row; j++) {
+            for (i = 0; i < column; i++) {
+                if (plane[i][j] == 1) {
+                    counter++;
+                }
+                if (plane[i][j] == 0) {
+                    nrow = i;
+                    ncol = j;
+                }
+                if (plane[i][j] == 2) {
+                    done = false;
+                }
+            }
+            if (done == true && counter > 1) {
+                marker[nrow][ncol] = 1;
+            }
+            if (counter > 2) {
+                win = true;
+            }
+            counter = 0;
+            done = true;
+        }
+    }
+
+    private static void dwnDiag() { //Checks down diagnol for nearby wins
+        int counter = 0;
+        int tracker = 0;
+        int nrow = 0;
+        int ncol = 0;
+        int i = 0;
+        int j = 0;
+        boolean done = true;
+        
+        while (tracker < row) {
+            if (plane[i][j] == 1) {
+                counter++;
+            }
+            if (plane[i][j] == 0) {
+                nrow = i;
+                ncol = j;
+            }
+            if (plane[i][j] == 2) {
+                done = false;
+            }
+            i++;
+            j++;
+            tracker++;
+        }
+        if (done == true && counter > 1) {
+            marker[nrow][ncol] = 1;
+        }
+        if (counter > 2) {
+            win = true;
+        }
+    }
+
+    private static void upDiag() { //Check up Diagnol for nearby wins
+        int counter = 0;
+        int tracker = 0;
+        int nrow = 0;
+        int ncol = 0;
+        int i = 0;
+        int j = 2;
+        boolean done = true;
+
+        while (tracker < row) {
+            if (plane[i][j] == 1) {
+                counter++;
+            }
+            if (plane[i][j] == 0) {
+                nrow = i;
+                ncol = j;
+            }
+            if (plane[i][j] == 2) {
+                done = false;
+            }
+            i++;
+            j--;
+            tracker++;
+        }
+        if (done == true && counter > 1) {
+            marker[nrow][ncol] = 1;
+        }
+        if (counter > 2) {
+            win = true;
+        }
+    }
+
+    private static void clear() { //Clears markers
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                marker[i][j] = 0;
+            }
+        }
+        another = false;
+    }
+
 }
